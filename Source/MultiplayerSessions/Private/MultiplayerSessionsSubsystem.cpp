25311497f9cb2f7f6c135808167818d963e0d7ad
Spr_Aachen <2835946988@qq.com>
2025-07-29 14:45:29 +0800

    Update MultiplayerSessionsSubsystem class

--- Source/MultiplayerSessions/Private/MultiplayerSessionsSubsystem.cpp
+++ Source/MultiplayerSessions/Private/MultiplayerSessionsSubsystem.cpp
@@ -3,6 +3,8 @@
 
 #include "MultiplayerSessionsSubsystem.h"
 #include "OnlineSubsystem.h"
+#include "OnlineSessionSettings.h"
+#include "Online/OnlineSessionNames.h"
 
 
 UMultiplayerSessionsSubsystem::UMultiplayerSessionsSubsystem() :
@@ -22,9 +24,8 @@ UMultiplayerSessionsSubsystem::UMultiplayerSessionsSubsystem() :
     StartSessionCompleteDelegate(
         FOnStartSessionCompleteDelegate::CreateUObject(this, &UMultiplayerSessionsSubsystem::OnStartSessionComplete)
     )
-
 {
-    // Get session interface
+    /* Get session interface */
     IOnlineSubsystem *OnlineSubsystem = IOnlineSubsystem::Get();
     if (OnlineSubsystem)
     {
@@ -35,37 +36,252 @@ UMultiplayerSessionsSubsystem::UMultiplayerSessionsSubsystem() :
 
 void UMultiplayerSessionsSubsystem::CreateSession(int32 NumPublicConnections, FString MatchType)
 {
+    /* If SessionInterface is not valid */
+    if (!SessionInterface.IsValid()) // The way to check if TSharedPtr is valid is by using the 'IsValid' function
+    {
+        return;
+    }
+
+    /* Check and destroy the existing session */
+    // Get the existing session pointer
+    auto ExistingSession = SessionInterface->GetNamedSession(
+        NAME_GameSession // If we always use this NAME_GameSession, then we'll always be checking to see if a session with this name exists
+    );
+	// Check to see if the existing session pointer is not null
+	if (ExistingSession != nullptr)
+	{
+        /* Destroy the existing session */
+        // SessionInterface->DestroySession(
+        //     NAME_GameSession // Same as above
+        // ); // We should be handling this call in DestroySession rather than accessing the SessionInterface directly
+        bCreateSessionOnDestroy = true;
+        LastNumPublicConnections = NumPublicConnections;
+        LastMatchType = MatchType;
+        DestroySession();
+    }
+
+	/* Create a new session */
+    // Add CreateSessionCompleteDelegate to SessionInterface's delegate list and store it in a FDelegateHandle
+    CreateSessionCompleteDelegateHandle = SessionInterface->AddOnCreateSessionCompleteDelegate_Handle(CreateSessionCompleteDelegate); // Once a session is created, then the fallback function which bound to this delegate will be called
+    // Initialize LastSessionSettings TSharedPtr to class FOnlineSessionSettings
+    LastSessionSettings = MakeShareable(new FOnlineSessionSettings());
+    /* Configure session settings */
+    LastSessionSettings->bIsLANMatch = IOnlineSubsystem::Get()->GetSubsystemName() == "Null" ? true : false; // Using ternary operator by checking SubsystemName to decide whether to connect over the internet
+    LastSessionSettings->NumPublicConnections = NumPublicConnections; // Determine how many players can connect to the game
+	LastSessionSettings->bAllowJoinInProgress = true; // Allow players to join when session is running
+    LastSessionSettings->bAllowJoinViaPresence = true; // Allow steam to search for sessions going on players' regions
+    LastSessionSettings->bShouldAdvertise = true; // Allow steam to advertise sessions
+	LastSessionSettings->bUsesPresence = true; // Allow players to find sessions going on their regions
+    //LastSessionSettings->bUseLobbiesIfAvailable = true; // Fix sessions finding issue
+    LastSessionSettings->Set( // Specify a match type so we can check that type once we've found the session
+        FName("MatchType"), // FName key to define a match type
+        MatchType, // FString value to define the match type
+        EOnlineDataAdvertisementType::ViaOnlineServiceAndPing // Session will be advertised via the online service and ping
+    );
+    LastSessionSettings->BuildUniqueId = 1; // Allow multiple users to launch their own build and host
+    /* Create a session */
+    // Get the world's first local player
+    const ULocalPlayer *LocalPlayer = GetWorld()->GetFirstLocalPlayerFromController();
+    bool IsCreationSuccessful = SessionInterface->CreateSession(
+        *LocalPlayer->GetPreferredUniqueNetId(),
+        NAME_GameSession,
+        *LastSessionSettings
+    );
+    // If session creation is failed
+    if (!IsCreationSuccessful)
+    {
+        // Remove CreateSessionCompleteDelegate from the list
+        SessionInterface->ClearOnCreateSessionCompleteDelegate_Handle(CreateSessionCompleteDelegateHandle);
+        // Broadcast custom multicast delegate
+        MultiplayerOnCreateSessionComplete.Broadcast(
+            false
+        );
+    }
 }
+
 
 void UMultiplayerSessionsSubsystem::OnCreateSessionComplete(FName SessionName, bool bWasSuccessful)
 {
+    if (SessionInterface)
+    {
+        // Remove CreateSessionCompleteDelegate from the list
+        SessionInterface->ClearOnCreateSessionCompleteDelegate_Handle(CreateSessionCompleteDelegateHandle);
+    }
+    // Broadcast custom multicast delegate
+    MultiplayerOnCreateSessionComplete.Broadcast(
+        bWasSuccessful
+    );
 }
 
 
 void UMultiplayerSessionsSubsystem::FindSessions(int32 MaxSearchResults)
 {
+    /* If SessionInterface is not valid */
+    if (!SessionInterface.IsValid()) // The way to check if TSharedPtr is valid is by using the 'IsValid' function
+	{
+		return;
+	}
+
+    /* Find game sessions */
+    // Add FindSessionsCompleteDelegate to SessionInterface's delegate list and store it in a FDelegateHandle
+    FindSessionsCompleteDelegateHandle = SessionInterface->AddOnFindSessionsCompleteDelegate_Handle(FindSessionsCompleteDelegate); // Once all sessions are found, then the fallback function which bound to this delegate will be called
+    // Initialize LastSessionSearch TSharedPtr to class FOnlineSessionSearch
+    LastSessionSearch = MakeShareable(new FOnlineSessionSearch);
+	// Configure search settings
+    LastSessionSearch->MaxSearchResults = MaxSearchResults;
+    LastSessionSearch->bIsLanQuery = IOnlineSubsystem::Get()->GetSubsystemName() == "Null" ? true : false; // Using ternary operator by checking SubsystemName to decide whether to connect over the internet
+    LastSessionSearch->QuerySettings.Set( // Make sure any session we find is using presence
+		SEARCH_PRESENCE, // Macro
+		true,
+		EOnlineComparisonOp::Equals
+	);
+    /* Find sessions */
+    // Get the world's first local player
+    const ULocalPlayer *LocalPlayer = GetWorld()->GetFirstLocalPlayerFromController();
+	bool IsSearchSuccessful = SessionInterface->FindSessions(
+		*LocalPlayer->GetPreferredUniqueNetId(),
+		LastSessionSearch.ToSharedRef()
+	);
+    // If sessions search is failed
+    if (!IsSearchSuccessful)
+    {
+        // Remove FindSessionsCompleteDelegate from the list
+        SessionInterface->ClearOnFindSessionsCompleteDelegate_Handle(FindSessionsCompleteDelegateHandle);
+        // Broadcast custom multicast delegate
+        MultiplayerOnFindSessionsComplete.Broadcast(
+            TArray<FOnlineSessionSearchResult>(), // Empty array
+            false
+        );
+    }
 }
+
 
 void UMultiplayerSessionsSubsystem::OnFindSessionsComplete(bool bWasSuccessful)
 {
+    if (SessionInterface)
+    {
+        // Remove FindSessionsCompleteDelegate from the list
+        SessionInterface->ClearOnFindSessionsCompleteDelegate_Handle(FindSessionsCompleteDelegateHandle);
+    }
+    // Broadcast custom multicast delegate
+    if (LastSessionSearch->SearchResults.Num() <= 0)
+    {
+        MultiplayerOnFindSessionsComplete.Broadcast(
+            TArray<FOnlineSessionSearchResult>(), // Empty array
+            false
+        );
+    }
+    else
+    {
+        MultiplayerOnFindSessionsComplete.Broadcast(
+            LastSessionSearch->SearchResults,
+            bWasSuccessful
+        );
+    }
 }
 
 
 void UMultiplayerSessionsSubsystem::JoinSession(const FOnlineSessionSearchResult &SessionResult)
 {
+	/* If SessionInterface is not valid */
+	if (!SessionInterface.IsValid()) // The way to check if TSharedPtr is valid is by using the 'IsValid' function
+    {
+        // Broadcast custom multicast delegate
+        MultiplayerOnJoinSessionsComplete.Broadcast(
+            EOnJoinSessionCompleteResult::UnknownError
+        );
+        return;
+    }
+
+    /* Join the game session */
+	// Add JoinSessionCompleteDelegate to SessionInterface's delegate list
+    JoinSessionCompleteDelegateHandle = SessionInterface->AddOnJoinSessionCompleteDelegate_Handle(JoinSessionCompleteDelegate); // Once the session is joined, then the fallback function which bound to this delegate will be called
+    /* Join session */
+    // Get the world's first local player
+    const ULocalPlayer *LocalPlayer = GetWorld()->GetFirstLocalPlayerFromController();
+	bool IsJointSuccessful = SessionInterface->JoinSession(
+		*LocalPlayer->GetPreferredUniqueNetId(),
+		NAME_GameSession,
+		SessionResult
+	);
+    // If sessions joint is failed
+    if (!IsJointSuccessful)
+    {
+        // Remove JoinSessionCompleteDelegate from the list
+        SessionInterface->ClearOnJoinSessionCompleteDelegate_Handle(JoinSessionCompleteDelegateHandle);
+        // Broadcast custom multicast delegate
+        MultiplayerOnJoinSessionsComplete.Broadcast(
+            EOnJoinSessionCompleteResult::UnknownError
+        );
+    }
 }
+
 
 void UMultiplayerSessionsSubsystem::OnJoinSessionComplete(FName SessionName, EOnJoinSessionCompleteResult::Type Result)
 {
+    if (SessionInterface)
+    {
+        // Remove JoinSessionCompleteDelegate from the list
+        SessionInterface->ClearOnJoinSessionCompleteDelegate_Handle(JoinSessionCompleteDelegateHandle);
+    }
+    // Broadcast custom multicast delegate
+    MultiplayerOnJoinSessionsComplete.Broadcast(
+        Result
+    );
 }
 
 
 void UMultiplayerSessionsSubsystem::DestroySession()
 {
+	/* If SessionInterface is not valid */
+	if (!SessionInterface.IsValid()) // The way to check if TSharedPtr is valid is by using the 'IsValid' function
+    {
+        // Broadcast custom multicast delegate
+        MultiplayerOnDestroySessionComplete.Broadcast(
+            false
+        );
+        return;
+    }
+
+    /* Destroy the game session */
+    // Add DestroySessionCompleteDelegate to SessionInterface's delegate list
+    DestroySessionCompleteDelegateHandle = SessionInterface->AddOnDestroySessionCompleteDelegate_Handle(DestroySessionCompleteDelegate); // Once the session is joined, then the fallback function which bound to this delegate will be called
+    /* Destroy session */
+    // Get the world's first local player
+    const ULocalPlayer *LocalPlayer = GetWorld()->GetFirstLocalPlayerFromController();
+    bool IsDestructionSuccessful = SessionInterface->DestroySession(
+        NAME_GameSession
+    );
+    // If sessions destruction is failed
+    if (!IsDestructionSuccessful)
+    {
+        // Remove DestroySessionCompleteDelegate from the list
+        SessionInterface->ClearOnDestroySessionCompleteDelegate_Handle(DestroySessionCompleteDelegateHandle);
+        // Broadcast custom multicast delegate
+        MultiplayerOnDestroySessionComplete.Broadcast(
+            false
+        );
+    }
 }
+
 
 void UMultiplayerSessionsSubsystem::OnDestroySessionComplete(FName SessionName, bool bWasSuccessful)
 {
+    if (SessionInterface)
+    {
+        // Remove DestroySessionCompleteDelegate from the list
+        SessionInterface->ClearOnDestroySessionCompleteDelegate_Handle(DestroySessionCompleteDelegateHandle);
+    }
+    // If new session creation is needed
+    if (bWasSuccessful && bCreateSessionOnDestroy)
+    {
+        bCreateSessionOnDestroy = false;
+        CreateSession(LastNumPublicConnections, LastMatchType);
+    }
+    // Broadcast custom multicast delegate
+    MultiplayerOnDestroySessionComplete.Broadcast(
+        bWasSuccessful
+    );
 }
 
 
--- Source/MultiplayerSessions/Public/MultiplayerSessionsSubsystem.h
+++ Source/MultiplayerSessions/Public/MultiplayerSessionsSubsystem.h
@@ -6,11 +6,45 @@
 #include "Subsystems/GameInstanceSubsystem.h"
 #include "Interfaces/OnlineSessionInterface.h"
 
+// Header files with '.generated' should be put in the end
 #include "MultiplayerSessionsSubsystem.generated.h"
 
-/**
- * 
- */
+
+/*
+Declare our own custom multicast delegates (which means once it's broadcast, multiple classes can bind their ufunctions to it)
+Notice the difference between DYNAMIC_MULTICAST_DELEGATE macro and MULTICAST_DELEGATE macro
+*/
+// Declare a dynamic multicast delegate that is capable of binding a ufunction that takes one parameter
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam( // No worry if having red squiggles, those will go away after compiling (debug anyway)
+	FMultiplayerOnCreateSessionComplete, // Decide on a name for the delegate
+	bool, // Indicate the input parameter type of the function that can be bound to the delegate
+	bWasSuccessful // Provide a name for the input parameter
+);
+// Declare a multicast delegate that is capable of binding a function that takes two parameters
+DECLARE_MULTICAST_DELEGATE_TwoParams(
+	FMultiplayerOnFindSessionsComplete, // Decide on a name for the delegate
+	const TArray<FOnlineSessionSearchResult>& SessionResults, // Indicate the first input parameter of the function that can be bound to the delegate
+	bool bWasSuccessful // Indicate the second input parameter of the function that can be bound to the delegate
+);
+// Declare a multicast delegate that is capable of binding a function that takes one parameter
+DECLARE_MULTICAST_DELEGATE_OneParam(
+	FMultiplayerOnJoinSessionsComplete, // Decide on a name for the delegate
+	EOnJoinSessionCompleteResult::Type Result // Indicate the input parameter of the function that can be bound to the delegate
+);
+// Declare a dynamic multicast delegate that is capable of binding a ufunction that takes one parameter
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam( // No worry if having red squiggles, those will go away after compiling (debug anyway)
+	FMultiplayerOnDestroySessionComplete, // Decide on a name for the delegate
+	bool, // Indicate the input parameter type of the function that can be bound to the delegate
+	bWasSuccessful // Provide a name for the input parameter
+);
+// Declare a dynamic multicast delegate that is capable of binding a ufunction that takes one parameter
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam( // No worry if having red squiggles, those will go away after compiling (debug anyway)
+	FMultiplayerOnStartSessionComplete, // Decide on a name for the delegate
+	bool, // Indicate the input parameter type of the function that can be bound to the delegate
+	bWasSuccessful // Provide a name for the input parameter
+);
+
+
 UCLASS()
 class MULTIPLAYERSESSIONS_API UMultiplayerSessionsSubsystem : public UGameInstanceSubsystem
 {
@@ -19,40 +53,14 @@ class MULTIPLAYERSESSIONS_API UMultiplayerSessionsSubsystem : public UGameInstan
 public:
 	UMultiplayerSessionsSubsystem();
 
-	/* Session functionality handler functions to be called by Menu class */
-	//
-	void CreateSession(
-		int32 NumPublicConnections, // Specify the number of players that can join the game
-		FString MatchType // 
-	);
-	//
-	void FindSessions(
-		int32 MaxSearchResults // Specify the number of search results
-	);
-	//
-	void JoinSession(
-		const FOnlineSessionSearchResult &SessionResult
-	);
-	//
-	void DestroySession();
-	//
-	void StartSession();
-
-protected:
-	/* Callback functions for the session delegates. Notice that each of their input&return params have to match the definition of the corresponding delegate */
-	// Callback function which will be called in response to successfully create a game session
-	void OnCreateSessionComplete(FName SessionName, bool bWasSuccessful);
-	// Callback function which will be called in response to successfully find game sessions
-	void OnFindSessionsComplete(bool bWasSuccessful);
-	// Callback function which will be called in response to successfully join the game session
-	void OnJoinSessionComplete(FName SessionName, EOnJoinSessionCompleteResult::Type Result);
-	// Callback function which will be called in response to successfully destroy the game session
-	void OnDestroySessionComplete(FName SessionName, bool bWasSuccessful);
-	// Callback function which will be called in response to successfully start the game session
-	void OnStartSessionComplete(FName SessionName, bool bWasSuccessful);
-
 private:
+	// Smart pointer to hold the online session interface
 	IOnlineSessionPtr SessionInterface;
+
+	// TSharedPtr to store the session settings that we last created
+	TSharedPtr<FOnlineSessionSettings> LastSessionSettings;
+	// TSharedPtr to store the session search that we last used
+	TSharedPtr<FOnlineSessionSearch> LastSessionSearch;
 
 	/* Session delegates to bind callback functions */
 	// Delegate to bind callback function of session creation
@@ -75,4 +83,56 @@ private:
 	FOnStartSessionCompleteDelegate StartSessionCompleteDelegate;
 	// DelegateHandle for StartSessionCompleteDelegate
 	FDelegateHandle StartSessionCompleteDelegateHandle;
+
+public:
+	/* Custom multicast delegates to bind callback functions */
+	// Dynamic multicast delegate to bind callback ufunctions of session creation result
+	FMultiplayerOnCreateSessionComplete MultiplayerOnCreateSessionComplete;
+	// Multicast delegate to bind callback functions of session search result
+	FMultiplayerOnFindSessionsComplete MultiplayerOnFindSessionsComplete;
+	// Multicast delegate to bind callback functions of session joint result
+	FMultiplayerOnJoinSessionsComplete MultiplayerOnJoinSessionsComplete;
+	// Dynamic multicast delegate to bind callback ufunctions of session destruction result
+	FMultiplayerOnDestroySessionComplete MultiplayerOnDestroySessionComplete;
+	// Dynamic multicast delegate to bind callback ufunctions of session start result
+	FMultiplayerOnStartSessionComplete MultiplayerOnStartSessionComplete;
+
+private:
+	/* Store these for the last session we failed to created */
+	bool bCreateSessionOnDestroy{false};
+	int32 LastNumPublicConnections;
+	FString LastMatchType;
+
+public:
+	/* Session functionality handler functions */
+	// Function to create game session
+	void CreateSession(
+		int32 NumPublicConnections, // Specify the number of players that can join the game
+		FString MatchType // Specify the match type
+	);
+	// Function to find game sessions
+	void FindSessions(
+		int32 MaxSearchResults // Specify the number of search results
+	);
+	// Function to join game session
+	void JoinSession(
+		const FOnlineSessionSearchResult &SessionResult
+	);
+	// Function to destroy game session
+	void DestroySession();
+	// Function to start game session
+	void StartSession();
+
+protected:
+	/* Callback functions for the session delegates. Notice that each of their input&return params have to match the definition of the corresponding delegate */
+	// Callback function which will be called in response to successfully create a game session
+	void OnCreateSessionComplete(FName SessionName, bool bWasSuccessful);
+	// Callback function which will be called in response to successfully find game sessions
+	void OnFindSessionsComplete(bool bWasSuccessful);
+	// Callback function which will be called in response to successfully join the game session
+	void OnJoinSessionComplete(FName SessionName, EOnJoinSessionCompleteResult::Type Result);
+	// Callback function which will be called in response to successfully destroy the game session
+	void OnDestroySessionComplete(FName SessionName, bool bWasSuccessful);
+	// Callback function which will be called in response to successfully start the game session
+	void OnStartSessionComplete(FName SessionName, bool bWasSuccessful);
 };
