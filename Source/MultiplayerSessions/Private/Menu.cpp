b3716b9081bfb6a6b41002c4bce7c50df3d70d5d
Spr_Aachen <2835946988@qq.com>
2025-07-31 06:56:53 +0800

    Update Menu class

--- Source/MultiplayerSessions/Private/Menu.cpp
+++ Source/MultiplayerSessions/Private/Menu.cpp
@@ -3,35 +3,33 @@
 
 #include "Menu.h"
 #include "Components/Button.h"
+#include "OnlineSessionSettings.h"
+#include "Interfaces/OnlineSessionInterface.h"
 
 
 void UMenu::HostButtonClicked()
 {
-    // Verify that the function callback is working
-    if (GEngine)
+    // Disable HostButton
+    HostButton->SetIsEnabled(false);
+    /* Create session */
+    // Check if MultiplayerSessionsSubsystem pointer is valid
+    if (MultiplayerSessionsSubsystem)
     {
-        // Show debug message
-        GEngine->AddOnScreenDebugMessage(
-            -1,
-            15.f,
-            FColor::Yellow,
-            FString(TEXT("Host Button Clicked"))
-        );
-    }
+        // Call CreateSession function
+        MultiplayerSessionsSubsystem->CreateSession(NumPublicConnections, MatchType);
+    }    
 }
 
 
 void UMenu::JoinButtonClicked()
 {
-    // Verify that the function callback is working
-    if (GEngine)
+    // Disable JoinButton
+    JoinButton->SetIsEnabled(false);
+    /* Find an active session */
+    if (MultiplayerSessionsSubsystem)
     {
-        // Show debug message
-        GEngine->AddOnScreenDebugMessage(
-            -1,
-            15.f,
-            FColor::Yellow,
-            FString(TEXT("Join Button Clicked"))
+        MultiplayerSessionsSubsystem->FindSessions(
+            10000 // Max session search results
         );
     }
 }
@@ -41,25 +39,148 @@ void UMenu::JoinButtonClicked()
 bool UMenu::Initialize()
 {
     // Call the super version
-    if (!Super::Initialize())
+    bool IsInitializationSuccessful = Super::Initialize();
+    // If initialization is failed
+    if (!IsInitializationSuccessful)
     {
         return false;
     }
-    // Bind callbacks to buttons
-    if (HostButton)
+    /* Bind callbacks to buttons */
+    else
     {
-        HostButton->OnClicked.AddDynamic(this, &UMenu::HostButtonClicked);
+        if (HostButton)
+        {
+            HostButton->OnClicked.AddDynamic(this, &UMenu::HostButtonClicked);
+        }
+        if (JoinButton)
+        {
+            JoinButton->OnClicked.AddDynamic(this, &UMenu::JoinButtonClicked);
+        }
+        return true;
     }
-    if (JoinButton)
-    {
-        JoinButton->OnClicked.AddDynamic(this, &UMenu::JoinButtonClicked);
-    }
-    return true;
 }
 
 
-void UMenu::MenuSetup()
+void UMenu::OnCreateSession(bool bWasSuccessful)
 {
+    if (bWasSuccessful)
+    {
+        // Print confirmation on the screen
+        if (GEngine)
+        {
+            // Show debug message
+            GEngine->AddOnScreenDebugMessage(
+                -1,
+                15.f,
+                FColor::Green,
+                FString(TEXT("Session created successfully!"))
+            );
+        }
+        /* Travel to lobby level and open it as a listen server */
+        UWorld *World = GetWorld();
+        if (World)
+        {
+            World->ServerTravel(
+				FString(PathToLobby)
+            );
+        }
+    }
+    else
+    {
+        // Print confirmation on the screen
+        if (GEngine)
+        {
+            // Show debug message
+            GEngine->AddOnScreenDebugMessage(
+                -1,
+                15.f,
+                FColor::Red,
+                FString(TEXT("Failed to create session!"))
+            );
+        }
+        // Enable HostButton
+        HostButton->SetIsEnabled(true);
+    }
+}
+
+
+void UMenu::OnFindSessions(const TArray<FOnlineSessionSearchResult> &SessionResults, bool bWasSuccessful)
+{
+    // If the MultiplayerSessionsSubsystem pointer is not null
+    if (MultiplayerSessionsSubsystem == nullptr)
+    {
+        return;
+    }
+    for (auto Result : SessionResults)
+    {
+        FString SettingsValue;
+        // Check if it has a matching key value pair that we are looking for
+        Result.Session.SessionSettings.Get(FName("MatchType"), MatchType);
+        // If it's the correct match type
+        if (SettingsValue == MatchType)
+        {
+            // Call JoinSession
+            MultiplayerSessionsSubsystem->JoinSession(Result);
+            return;
+        }
+    }
+    // Enable JoinButton if failed to find session or no session is found
+    if (!bWasSuccessful || SessionResults.Num() == 0)
+    {
+        JoinButton->SetIsEnabled(true);
+    }
+}
+
+
+void UMenu::OnJoinSession(EOnJoinSessionCompleteResult::Type Result)
+{
+    /* Get session interface */
+    IOnlineSubsystem *Subsystem = IOnlineSubsystem::Get();
+    if (Subsystem)
+    {
+        // Make a local pointer for session interface
+        IOnlineSessionPtr SessionInterface = Subsystem->GetSessionInterface();
+        /* If OnlineSessionInterface is valid */
+        if (SessionInterface.IsValid()) // The way to check if TSharedPtr is valid is by using the 'IsValid' function
+        {
+            FString Address;
+            bool AddressExist = SessionInterface->GetResolvedConnectString(NAME_GameSession, Address); // This function will fill in that string with the address we need
+            // Call ClientTravel
+            APlayerController *PlayerController = GetGameInstance()->GetFirstLocalPlayerController();
+            if (PlayerController)
+            {
+                PlayerController->ClientTravel(
+                    Address,
+                    ETravelType::TRAVEL_Absolute
+                );
+            }
+        }
+    }
+    // Enable JoinButton if failed to join session
+    if (Result != EOnJoinSessionCompleteResult::Success)
+    {
+        JoinButton->SetIsEnabled(true);
+    }
+}
+
+
+void UMenu::OnDestroySession(bool bWasSuccessful)
+{
+}
+
+
+void UMenu::OnStartSession(bool bWasSuccessful)
+{
+}
+
+
+void UMenu::MenuSetup(int32 NumberOfPublicConnections, FString TypeOfMatch, FString LobbyPath)
+{
+    /* Set member variables with inputs */
+    NumPublicConnections = NumberOfPublicConnections;
+    MatchType = TypeOfMatch;
+    PathToLobby = FString::Printf(TEXT("%s?listen"), *LobbyPath); // 
+
     // Add the widget to viewport
     AddToViewport();
     // Set the widget's visibility to visible
@@ -70,12 +191,12 @@ void UMenu::MenuSetup()
     /* Set the input mode via player controller */
     // Get World
     UWorld *World = GetWorld();
-    // Check to see if World is valid
+    // If World is valid
     if (World)
     {
         // Get the World's first player controller
         APlayerController *PlayerController = World->GetFirstPlayerController();
-        // Check to see if player controller exists
+        // If player controller exists
         if (PlayerController)
         {
             // That way once our widget is created and we're calling menu setup, we'll set the input mode so that we're not applying input to any pawns in the world but just focus on the UI
@@ -90,4 +211,63 @@ void UMenu::MenuSetup()
             PlayerController->SetShowMouseCursor(true);
         }
     }
+
+    UGameInstance *GameInstance = GetGameInstance();
+    if (GameInstance)
+    {
+        // Get subsystem and store it in MultiplayerSessionsSubsystem pointer, through this subsystem we can call functions created on subsystem class
+        MultiplayerSessionsSubsystem = GameInstance->GetSubsystem<UMultiplayerSessionsSubsystem>(); // GetSubsystem requires a template param
+    }
+
+    if (MultiplayerSessionsSubsystem)
+    {
+        /* Bind callback functions to multicast delegates */
+        MultiplayerSessionsSubsystem->MultiplayerOnCreateSessionComplete.AddDynamic(this, &UMenu::OnCreateSession);
+        MultiplayerSessionsSubsystem->MultiplayerOnFindSessionsComplete.AddUObject(this, &UMenu::OnFindSessions);
+        MultiplayerSessionsSubsystem->MultiplayerOnJoinSessionsComplete.AddUObject(this, &UMenu::OnJoinSession);
+        MultiplayerSessionsSubsystem->MultiplayerOnDestroySessionComplete.AddDynamic(this, &UMenu::OnDestroySession);
+        MultiplayerSessionsSubsystem->MultiplayerOnStartSessionComplete.AddDynamic(this, &UMenu::OnStartSession);
+    }
+}
+
+
+void UMenu::MenuTearDown()
+{
+    // Remove the widget from the viewport
+    RemoveFromParent();
+    /* Reset the input mode */
+    // Get World
+    UWorld *World = GetWorld();
+    // If World is valid
+    if (World)
+    {
+        // Get player controller from the world
+        APlayerController *PlayerController = World->GetFirstPlayerController();
+        /* Set input mode */
+        // If PlayerController is valid
+        if (PlayerController)
+        {
+            // That way player input / player controller will respond to our user input
+            FInputModeGameOnly InputModeData;
+            // Set the input mode
+            PlayerController->SetInputMode(InputModeData);
+            // Set the cursor to be invisible
+            PlayerController->SetShowMouseCursor(false);
+            
+        }
+    }
+}
+
+
+/*void UMenu::OnLevelRemovedFromWorld(ULevel *InLevel, UWorld *InWorld)
+{
+    MenuTearDown();
+    Super::OnLevelRemovedFromWorld(InLevel, InWorld);
+}*/
+void UMenu::NativeDestruct()
+{
+    // Remove the widget from the viewport and reset the input mode
+    MenuTearDown();
+    // Call the super version
+    Super::NativeDestruct();
 }
--- Source/MultiplayerSessions/Public/Menu.h
+++ Source/MultiplayerSessions/Public/Menu.h
@@ -4,6 +4,10 @@
 
 #include "CoreMinimal.h"
 #include "Blueprint/UserWidget.h"
+
+#include "MultiplayerSessionsSubsystem.h"
+
+// Header files with '.generated' should be put in the end
 #include "Menu.generated.h"
 
 
@@ -13,6 +17,16 @@ class MULTIPLAYERSESSIONS_API UMenu : public UUserWidget
 	GENERATED_BODY()
 
 private:
+	// The subsystem designed to handle all online session functionality
+	UMultiplayerSessionsSubsystem *MultiplayerSessionsSubsystem; // Or use forward declaration if not willing to include header file
+
+	// Number of public connections
+	int32 NumPublicConnections{4};
+	// Match type
+	FString MatchType{TEXT("FreeForAll")};
+	// Lobby path
+	FString PathToLobby{TEXT("")}; // Initialize it with empty string so that we can give this a valid value later
+
 	// Link to the HostButton that exists on widget blueprint
 	UPROPERTY(meta = (BindWidget)) // While using BindWidget meta specifier, note that the variable has to have the same name as the button on widget blueprint
 	class UButton *HostButton; // Forward declare so there's no need to include the buttons' header file
@@ -33,8 +47,32 @@ protected:
 	// Override the inherited 'Initialize' virtual function on UUserWidget class to bind callback functions, this is because that `Initialize` is kind of like the constructor which will create the widget
 	virtual bool Initialize() override;
 
+protected:
+	/* Callbacks for the custom delegates on the MultiplayerSessionsSubsystem */
+	// Callback function which will be called when delegate is broadcast
+	UFUNCTION() // Because we're binding this to a dynamic multicast delegate
+	void OnCreateSession(bool bWasSuccessful);
+	// Callback function which will be called when delegate is broadcast
+	void OnFindSessions(const TArray<FOnlineSessionSearchResult> &SessionResults, bool bWasSuccessful);
+	// Callback function which will be called when delegate is broadcast
+	void OnJoinSession(EOnJoinSessionCompleteResult::Type Result);
+	// Callback function which will be called when delegate is broadcast
+	UFUNCTION()
+	void OnDestroySession(bool bWasSuccessful);
+	// Callback function which will be called when delegate is broadcast
+	UFUNCTION()
+	void OnStartSession(bool bWasSuccessful);
+
 public:
 	// Blueprint callable function to setup menu
 	UFUNCTION(BlueprintCallable)
-	void MenuSetup();
+	void MenuSetup(int32 NumberOfPublicConnections = 4, FString TypeOfMatch = FString(TEXT("FreeForAll")), FString LobbyPath = FString(TEXT("/Game/ThirdPerson/Maps/Lobby"))); // By giving default values, inputs won't be requirements
+
+private:
+	// Function to remove the widget from the viewport and reset the input mode
+	void MenuTearDown();
+
+protected:
+	// Override the inherited 'NativeDestruct' virtual function on UUserWidget class to call MenuTearDown function, this function gets called when the level is removed form the world which happens when traveling to another level
+	virtual void NativeDestruct() override;
 };
